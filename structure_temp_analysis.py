a3c58683888aff5ddd4051f425f02b0189bc9da6
Deepak Mahudeswaran <m.deepak@ymail.com>
2018-11-10 15:31:29 -0700

    cascade number analysis

--- structure_temp_analysis.py
+++ structure_temp_analysis.py
@@ -5,6 +5,35 @@ import numpy as np
 from preprocess_data import load_prop_graph
 from util.util import tweet_node
 
+def get_num_cascade(node:tweet_node, edge_type="retweet"):
+    if edge_type == "retweet":
+        return len(node.retweet_children)
+    elif edge_type == "reply":
+        return len(node.reply_children)
+    else:
+        return len(node.children)
+
+
+def get_temp_num_cascade(node: tweet_node, edge_type ="retweet", max_time=time.time()):
+
+    if edge_type == "retweet":
+        children = node.retweet_children
+    elif edge_type == "reply":
+        children = node.reply_children
+    else:
+        children = node.children
+
+    cascade_count = 0
+
+    for child in children:
+        if child.created_time <= max_time:
+            cascade_count += 1
+
+    return cascade_count
+
+
+
+
 
 def get_tree_height(node, edge_type="retweet"):
     if node is None:
@@ -96,11 +125,36 @@ def get_temporal_tree_height(node: tweet_node, edge_type="retweet", max_time=tim
     return max_child_height + 1
 
 
+def get_num_cascades_by_time(prop_graphs: list, edge_type: str, time_interval_sec: list):
+    temporal_num_cascades = []
+    for news_node in prop_graphs:
+        temp_cascade_num = []
+        first_post_time = get_first_post_time(news_node)
+        for time_limit in time_interval_sec:
+            node_count = get_temp_num_cascade(news_node, edge_type, first_post_time + time_limit)
+            temp_cascade_num.append(node_count)
+
+        temporal_num_cascades.append(temp_cascade_num)
+
+    return temporal_num_cascades
+
+
 def analyze_height(news_graphs: list, edge_type):
     heights = []
 
     for news_node in news_graphs:
         heights.append(get_tree_height(news_node, edge_type))
+
+    print("max", max(heights))
+    print("min", min(heights))
+    print("avg", np.mean(heights))
+
+
+def analyze_cascade(news_graphs: list, edge_type):
+    heights = []
+
+    for news_node in news_graphs:
+        heights.append(get_num_cascade(news_node, edge_type))
 
     print("max", max(heights))
     print("min", min(heights))
@@ -146,6 +200,21 @@ def analyze_height_by_time(prop_graphs: list, edge_type: str, time_interval_sec:
         print(flush=True)
 
 
+def analyze_cascade_num_by_time(prop_graphs: list, edge_type: str, time_interval_sec: list):
+    temporal_cascade_num = get_num_cascades_by_time(prop_graphs, edge_type, time_interval_sec)
+
+    temporal_cascade_num = np.array([np.array(val) for val in temporal_cascade_num])
+
+    for idx, time_limit_sec in enumerate(time_interval_sec):
+        heights_at_time = temporal_cascade_num[:, idx]
+        print("Time limit: {}".format(time_limit_sec))
+        print("Min num cascade : {}".format(np.min(heights_at_time)))
+        print("Max num cascade : {}".format(np.max(heights_at_time)))
+        print("Mean num cascade : {}".format(np.mean(heights_at_time)))
+        print(flush=True)
+
+
+
 def analyze_node_size_by_time(prop_graphs: list, edge_type: str, time_interval_sec: list):
     temporal_tree_node_sizes = get_node_size_by_time(prop_graphs, edge_type, time_interval_sec)
 
@@ -175,9 +244,13 @@ if __name__ == "__main__":
     RETWEET_EDGE = "retweet"
     REPLY_EDGE = "reply"
 
-    analyze_node_count(propagation_graphs, RETWEET_EDGE)
-    analyze_node_size_by_time(propagation_graphs, RETWEET_EDGE, [60, 300, 600, 900, 18000, 36000, 72000])
+    analyze_cascade(propagation_graphs, RETWEET_EDGE)
+    analyze_cascade_num_by_time(propagation_graphs, RETWEET_EDGE, [60, 300, 600, 900, 18000, 36000, 72000]
+                                )
 
     exit(1)
     analyze_height(propagation_graphs, RETWEET_EDGE)
-    analyze_height_by_time(propagation_graphs, "retweet", [60, 300, 600, 900, 18000, 36000, 72000])
+    analyze_height_by_time(propagation_graphs, RETWEET_EDGE, [60, 300, 600, 900, 18000, 36000, 72000])
+
+    analyze_node_count(propagation_graphs, RETWEET_EDGE)
+    analyze_node_size_by_time(propagation_graphs, RETWEET_EDGE, [60, 300, 600, 900, 18000, 36000, 72000])
